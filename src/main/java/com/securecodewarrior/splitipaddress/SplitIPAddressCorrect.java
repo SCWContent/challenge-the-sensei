fd48950c9e8b51f7471a2c74cce4e6f3c27d9b0c
eviltester <arichardson@securecodewarrior.com>
2021-03-02 14:23:22 +0000

    fixed code typo

--- src/main/java/com/securecodewarrior/splitipaddress/SplitIPAddressCorrect.java
+++ src/main/java/com/securecodewarrior/splitipaddress/SplitIPAddressCorrect.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class SplitIPAddressCorrect {
 
     public static void main(String[] args) {
-	    String[] octets = "192,.168.1.2".split("\\.");
+	    String[] octets = "192.168.1.2".split("\\.");
 	    System.out.println(Arrays.toString(octets));
     }
 }
